68eb8bc348b6068e90d7e806a40eba869999795f
Stanley <stanley.traub@gmail.com>
2020-05-13 16:51:34 -0700

    Initial commit

---
+++ fetchjson/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+axios.get(url).then((res) => {
+    const todo = res.data as Todo
+    const {id} = todo
+    const {title} = todo
+    const {completed} = todo
+
+    logTodo(id, title, completed)
+
+})
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `)
+}
